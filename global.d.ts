1bf4dd594210b6f389358d3a3021875ffd563dbe
René Baumgärtner <terminator1000000@web.de>
2019-03-18 21:48:10 +0100

    provide a first configuration

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,20 @@
 // Collects all properties of a class except native functions and wraps them in an object with their types
 declare type NonFunctionPropertyNames<T> = { [K in keyof T]: T[K] extends () => any ? never : K }[keyof T];
 declare type ConstParams<T> = Pick<T, NonFunctionPropertyNames<T>>;
+
+type wsVerifyClientInfo = { origin: string; secure: boolean; req: IncomingMessage };
+type wsVerifyClientDone = (
+    res: boolean,
+    code?: number | undefined,
+    message?: string | undefined,
+    headers?: OutgoingHttpHeaders | undefined
+) => void;
+
+namespace Express {
+
+    interface Request {
+        // Overwrite Express session because it's always available
+        session: Express.Session;
+        sessionID: string;
+    }
+}
