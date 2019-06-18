66533a6e2c42b98758f8e79d6085618c350b9182
René Baumgärtner <terminator1000000@web.de>
2019-06-18 14:55:27 +0200

    end anoying error restarts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,34 +20,36 @@ let defaults = {
     source_map_support: true,
     max_memory_restart: '1G'
 };
-module.exports = JSON.parse(JSON.stringify({
-    apps: [lodash.merge({}, defaults, {
-        name: 'GameServer',
-        "node_args": ["--inspect=7000"],
-        script: 'out/app/GameServer.js',
-        pid_file: "var/pids/gameServer.pid",
-        error: "var/logs/gameServerError.log",
-        env: {
-            NODE_ENV: 'development',
-            PORT: 3000
-        },
-        env_production: {
-            NODE_ENV: 'production',
-            PORT: 8080
-        }
-    }), lodash.merge({}, defaults, {
-        name: 'WebServer',
-        "node_args": ["--inspect=7001"],
-        script: 'out/app/WebServer.js',
-        pid_file: "var/pids/webServer.pid",
-        error: "var/logs/webServerError.log",
-        env: {
-            NODE_ENV: 'development',
-            PORT: 3001
-        },
-        env_production: {
-            NODE_ENV: 'production',
-            PORT: 8081
-        }
-    })]
-}));
+let gameServer = lodash.merge({}, defaults, {
+    name: 'GameServer',
+    "node_args": ["--inspect=7000"],
+    script: 'out/app/GameServer.js',
+    pid_file: "var/pids/gameServer.pid",
+    error: "var/logs/gameServerError.log",
+    env: {
+        NODE_ENV: 'development',
+        PORT: 3000
+    },
+    env_production: {
+        NODE_ENV: 'production',
+        PORT: 8080
+    }
+});
+let webServer = lodash.merge({}, defaults, {
+    name: 'WebServer',
+    "node_args": ["--inspect=7001"],
+    script: 'out/app/WebServer.js',
+    pid_file: "var/pids/webServer.pid",
+    error: "var/logs/webServerError.log",
+    env: {
+        NODE_ENV: 'development',
+        PORT: 3001
+    },
+    env_production: {
+        NODE_ENV: 'production',
+        PORT: 8081
+    }
+});
+module.exports = {
+    apps: [gameServer, webServer]
+};
