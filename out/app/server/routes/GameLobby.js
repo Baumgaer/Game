e07f98a8f9cea689f98a714a9bd97ced2a7c75ab
René Baumgärtner <terminator1000000@web.de>
2020-04-18 16:23:56 +0200

    cleanup repo

--- out/app/server/routes/GameLobby.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const BDOGameLobby_1 = require("~bdo/routes/BDOGameLobby");
-const ServerRoute_1 = require("~server/lib/ServerRoute");
-class GameLobby extends BDOGameLobby_1.BDOGameLobbyFactory(ServerRoute_1.ServerRoute) {
-}
-exports.default = GameLobby;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiR2FtZUxvYmJ5LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vLi4vc291cmNlL2FwcC9zZXJ2ZXIvcm91dGVzL0dhbWVMb2JieS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLDJEQUErRDtBQUMvRCx5REFBc0Q7QUFTdEQsTUFBcUIsU0FBVSxTQUFRLGtDQUFtQixDQUFDLHlCQUFXLENBQUM7Q0FBSTtBQUEzRSw0QkFBMkUifQ==
--- out/app/server/routes/Home.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const BDOHome_1 = require("~bdo/routes/BDOHome");
-const ServerRoute_1 = require("~server/lib/ServerRoute");
-class Home extends BDOHome_1.BDOHomeFactory(ServerRoute_1.ServerRoute) {
-}
-exports.default = Home;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSG9tZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uL3NvdXJjZS9hcHAvc2VydmVyL3JvdXRlcy9Ib21lLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsaURBQXFEO0FBQ3JELHlEQUFzRDtBQVN0RCxNQUFxQixJQUFLLFNBQVEsd0JBQWMsQ0FBQyx5QkFBVyxDQUFDO0NBQUk7QUFBakUsdUJBQWlFIn0=
