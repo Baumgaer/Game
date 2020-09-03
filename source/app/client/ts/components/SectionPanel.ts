ad64166ef49a129b4a40e822584fc465d73616e8
René Baumgärtner <terminator1000000@web.de>
2020-09-03 16:13:25 +0200

    cleanup

--- source/app/client/ts/components/SectionPanel.ts
+++ source/app/client/ts/components/SectionPanel.ts
@@ -1,9 +1,7 @@
 import { BaseComponentFactory } from '~client/lib/BaseComponent';
-import { baseConstructor, attribute } from '~bdo/utils/decorators';
-import { Logger } from "~client/lib/Logger";
+import { baseConstructor } from '~bdo/utils/decorators';
 import style from "~static/less/components/SectionPanel.less";
 
-const logger = new Logger();
 /**
  * Manages routing on client side, switches views and collects routes for client.
  *
@@ -13,20 +11,6 @@ const logger = new Logger();
 @baseConstructor()
 export default class SectionPanel extends BaseComponentFactory(HTMLElement) {
 
-
-    @attribute() public aligned: string = "";
-
     styleString = style;
-
-    /**
-     * @inheritdoc
-     *
-     * @protected
-     * @memberof GameView
-     */
-    protected connectedCallback() {
-        super.connectedCallback();
-        logger.log("SectionPanel connected");
-    }
 
 }
